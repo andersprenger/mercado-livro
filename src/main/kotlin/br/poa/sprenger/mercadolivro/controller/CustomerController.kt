cc33f00278688be919c19bb66e571a15588e574f
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2022-01-18 17:54:26 -0300

    create first getmapping

---
+++ src/main/kotlin/br/poa/sprenger/mercadolivro/controller/CustomerController.kt
@@ -0,0 +1,12 @@
+package br.poa.sprenger.mercadolivro.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class CustomerController {
+    @GetMapping
+    fun helloWorld(): String {
+        return "hello world"
+    }
+}
