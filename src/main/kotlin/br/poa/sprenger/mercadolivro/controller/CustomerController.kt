57f0bef9a4cf10b17c3f9e0737e93a066439095f
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2022-01-21 00:27:19 -0300

    camada service

--- src/main/kotlin/br/poa/sprenger/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/br/poa/sprenger/mercadolivro/controller/CustomerController.kt
@@ -3,57 +3,41 @@ package br.poa.sprenger.mercadolivro.controller
 import br.poa.sprenger.mercadolivro.controller.request.PostCustomerRequest
 import br.poa.sprenger.mercadolivro.controller.request.PutCustomerRequest
 import br.poa.sprenger.mercadolivro.model.CustomerModel
+import br.poa.sprenger.mercadolivro.service.CustomerService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("customers")
-class CustomerController {
-
-    val customers = mutableListOf<CustomerModel>()
+class CustomerController(val service: CustomerService) {
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     fun getAll(@RequestParam name: String?): List<CustomerModel> {
-        name?.let {
-            return customers.filter { it.name.contains(name, true) }
-        }
-        return customers
-    }
-
-    @GetMapping("/{id}")
-    fun getCustomer(@PathVariable id: Int): CustomerModel {
-        try {
-            return customers[id - 1]
-        } catch (e: IndexOutOfBoundsException) {
-            throw ResponseStatusException(HttpStatus.BAD_REQUEST, e.localizedMessage)
-        }
-    }
-
-    @PutMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun updateCustomer(@PathVariable id: Int, @RequestBody customer: PutCustomerRequest) {
-        try {
-            customers.first {it.id == "$id"}.let {
-                it.name = customer.name
-                it.email = customer.email
-            }
-        } catch (e: NoSuchElementException) {
-            throw ResponseStatusException(HttpStatus.BAD_REQUEST, e.localizedMessage)
-        }
+        return service.getAll(name)
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun createCustomer(@RequestBody customer: PostCustomerRequest) {
-        customers.add(CustomerModel("${customers.size + 1}", customer.name, customer.email))
+        service.createCustomer(customer)
     }
 
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun getCustomer(@PathVariable id: Int): CustomerModel {
+        return service.getCustomer(id)
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun updateCustomer(@PathVariable id: Int, @RequestBody customer: PutCustomerRequest) {
+        return service.updateCustomer(id, customer)
+    }
+
+    @ResponseStatus(HttpStatus.ACCEPTED)
     @DeleteMapping("/{id}")
-    fun delete(@PathVariable id: String) {
-        val removed = customers.removeIf { it.id == id }
-        if (!removed) {
-            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "There's not any element with this id.")
-        }
+    fun delete(@PathVariable id: Int) {
+        service.delete(id)
     }
 }
---
+++ src/main/kotlin/br/poa/sprenger/mercadolivro/service/CustomerService.kt
@@ -0,0 +1,38 @@
+package br.poa.sprenger.mercadolivro.service
+
+import br.poa.sprenger.mercadolivro.controller.request.PostCustomerRequest
+import br.poa.sprenger.mercadolivro.controller.request.PutCustomerRequest
+import br.poa.sprenger.mercadolivro.model.CustomerModel
+import org.springframework.stereotype.Service
+
+@Service
+class CustomerService {
+    val customers = mutableListOf<CustomerModel>()
+
+    fun getAll(name: String?): List<CustomerModel> {
+        name?.let {
+            return customers.filter { it.name.contains(name, true) }
+        }
+
+        return customers
+    }
+
+    fun getCustomer(id: Int): CustomerModel {
+        return customers[id - 1]
+    }
+
+    fun updateCustomer(id: Int, customer: PutCustomerRequest) {
+        customers.first { it.id == "$id" }.let {
+            it.name = customer.name
+            it.email = customer.email
+        }
+    }
+
+    fun createCustomer(customer: PostCustomerRequest) {
+        customers.add(CustomerModel("${customers.size + 1}", customer.name, customer.email))
+    }
+
+    fun delete(id: Int) {
+        customers.removeIf { it.id == "$id" }
+    }
+}
