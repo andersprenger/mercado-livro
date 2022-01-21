b34fbfa69d8fef89705c0546399342f1acc105f2
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2022-01-20 21:31:33 -0300

    customer model

--- src/main/kotlin/br/poa/sprenger/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/br/poa/sprenger/mercadolivro/controller/CustomerController.kt
@@ -1,20 +1,16 @@
 package br.poa.sprenger.mercadolivro.controller
 
+import br.poa.sprenger.mercadolivro.model.CustomerModel
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("customer")
+@RequestMapping("customers")
 class CustomerController {
 
     @GetMapping
-    fun helloWorld(): String {
-        return "Customer 1"
-    }
-
-    @GetMapping("/2")
-    fun customer2(): String {
-        return "customer2"
+    fun getCustomer(): CustomerModel {
+        return CustomerModel("1","Anderson", "email@email.com")
     }
 }
---
+++ src/main/kotlin/br/poa/sprenger/mercadolivro/model/CustomerModel.kt
@@ -0,0 +1,7 @@
+package br.poa.sprenger.mercadolivro.model
+
+data class CustomerModel(
+    var id: String,
+    var name: String,
+    var email: String
+)
